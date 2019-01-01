9c8052c7c54f6fb3b4ecab6bce1cc4160b82fde1
Tachid Boonpipat <tanuki360@hotmail.com>
2019-01-02 00:20:13 +0700

    Implement simple firebase Auth UI and Google map API Geolocation

---
+++ fd/src/helpers/firebaseConfig.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+ export const config = { 
+      apiKey: "AIzaSyAy0CauI40fU0x9p2yo7TrinvfLy773jdg",
+      authDomain: "friend-drop.firebaseapp.com",
+      databaseURL: "https://friend-drop.firebaseio.com",
+      projectId: "friend-drop",
+      storageBucket: "friend-drop.appspot.com",
+      messagingSenderId: "621509072046"
+    }
